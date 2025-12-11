14dd17756b7fb94e7826f40170f947085007a2e9
mifumi323 <mifumi323@gmail.com>
2025-12-11 23:31:08 +0900

    プロジェクトファイルを追加。

---
+++ AvaloniaHideTitleBarSample/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaHideTitleBarSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
